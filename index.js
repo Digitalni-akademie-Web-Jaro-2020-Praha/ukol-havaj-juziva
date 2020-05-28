e3de695b86ee5b499816579fdaac875d6216f64b
Iva <ivajuzova@gmail.com>
2020-05-28 14:16:15 +0200

    Minor changes in index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let osoba1 = {
   jmeno: 'Alena',
-  uspory: 3000
+  uspory: 333000
 };
 let osoba2 = {
   jmeno: 'Karolína',
-  uspory: 68000
+  uspory: 8000
 };
 
 const dovolena = () => {
@@ -13,21 +13,22 @@ const dovolena = () => {
     if (osoba1.uspory >= 50000) {
       console.log(`Aleno, máš naspořeno min. 50 000 Kč, konkrétně ${osoba1.uspory} Kč.`);
     } else {
-      console.log(`Aleno, do 50 000 Kč ti chybí ještě ${50000 - osoba1.uspory} Kč.`);
+      console.log(`Aleno, tobě do 50 000 Kč chybí ještě ${50000 - osoba1.uspory} Kč.`);
     } if (osoba2.uspory >= 50000) {
       console.log(`Karolíno, máš naspořeno min. 50 000 Kč, konkrétně ${osoba2.uspory} Kč.`);
     } else {
-      console.log(`Karolíno, do 50 000 Kč ti chybí ještě ${50000 - osoba2.uspory} Kč.`)
+      console.log(`Karolíno, tobě do 50 000 Kč chybí ještě ${50000 - osoba2.uspory} Kč.`)
     }
   } else {
     console.log('Holky, zatím nemáte dohromady naspořeno ani 100 000 Kč. Ještě trochu musíte zamakat!')
     console.log(`Ještě vám chybí min. ${100000 - (osoba1.uspory + osoba2.uspory)} Kč.`)
 
     if (osoba1.uspory < 50000) {
-      console.log(`Aleno, do 50 000 Kč ti ještě chybí ${50000 - osoba1.uspory} Kč.`)
+      console.log(`Aleno, tobě do 50 000 Kč ještě chybí ${50000 - osoba1.uspory} Kč.`)
     } if (osoba2.uspory < 50000) {
-      console.log(`Karolíno, do 50 000 Kč ti ještě chybí ${50000 - osoba2.uspory} Kč.`)
+      console.log(`Karolíno, tobě do 50 000 Kč ještě chybí ${50000 - osoba2.uspory} Kč.`)
     }
+
   }
 }
 
