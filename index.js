df2096e97e00aabaafc8ac0337859f36f4b80d68
Iva <ivajuzova@gmail.com>
2020-06-05 11:27:55 +0200

    fixing hw6, consts added, function parameters added

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 let osoba1 = {
   jmeno: 'Alena',
-  uspory: 333000
+  uspory: 33000
 };
 let osoba2 = {
   jmeno: 'Karolína',
-  uspory: 8000
+  uspory: 288000
 };
 
-const dovolena = () => {
-  if (osoba1.uspory + osoba2.uspory >= 100000) {
-    console.log('Holky, máte už naspořeno alespoň 100 000 Kč, můžete vyrazit na dovolenou!');
-    if (osoba1.uspory >= 50000) {
-      console.log(`Aleno, máš naspořeno min. 50 000 Kč, konkrétně ${osoba1.uspory} Kč.`);
+const enoughBoth = 100000;
+const enoughOne = 50000;
+
+const dovolena = (cestovatel1, cestovatel2) => {
+  const funds1 = cestovatel1.uspory;
+  const funds2 = cestovatel2.uspory;
+  if (funds1 + funds2 >= enoughBoth) {
+    console.log(`Holky, máte už naspořeno alespoň ${enoughBoth} Kč, můžete vyrazit na dovolenou!`);
+    if (funds1 >= enoughOne) {
+      console.log(`Aleno, máš naspořeno min. ${enoughOne} Kč, konkrétně ${funds1} Kč.`);
     } else {
-      console.log(`Aleno, tobě do 50 000 Kč chybí ještě ${50000 - osoba1.uspory} Kč.`);
-    } if (osoba2.uspory >= 50000) {
-      console.log(`Karolíno, máš naspořeno min. 50 000 Kč, konkrétně ${osoba2.uspory} Kč.`);
+      console.log(`Aleno, tobě do ${enoughOne} Kč chybí ještě ${enoughOne - funds1} Kč.`);
+    } if (funds2 >= enoughOne) {
+      console.log(`Karolíno, máš naspořeno min. ${enoughOne} Kč, konkrétně ${funds2} Kč.`);
     } else {
-      console.log(`Karolíno, tobě do 50 000 Kč chybí ještě ${50000 - osoba2.uspory} Kč.`)
+      console.log(`Karolíno, tobě do ${enoughOne} Kč chybí ještě ${enoughOne - funds2} Kč.`)
     }
   } else {
-    console.log('Holky, zatím nemáte dohromady naspořeno ani 100 000 Kč. Ještě trochu musíte zamakat!')
-    console.log(`Ještě vám chybí min. ${100000 - (osoba1.uspory + osoba2.uspory)} Kč.`)
+    console.log(`Holky, zatím nemáte dohromady naspořeno ani ${enoughBoth} Kč. Ještě trochu musíte zamakat!`)
+    console.log(`Ještě vám chybí min. ${enoughBoth - (funds1 + funds2)} Kč.`)
 
-    if (osoba1.uspory < 50000) {
-      console.log(`Aleno, tobě do 50 000 Kč ještě chybí ${50000 - osoba1.uspory} Kč.`)
-    } if (osoba2.uspory < 50000) {
-      console.log(`Karolíno, tobě do 50 000 Kč ještě chybí ${50000 - osoba2.uspory} Kč.`)
+    if (funds1 < enoughOne) {
+      console.log(`Aleno, tobě do ${enoughOne} Kč ještě chybí ${enoughOne - funds1} Kč.`)
+    } if (funds2 < enoughOne) {
+      console.log(`Karolíno, tobě do ${enoughOne} Kč ještě chybí ${enoughOne - funds2} Kč.`)
     }
 
   }
 }
 
-dovolena();
+dovolena(osoba1, osoba2);
